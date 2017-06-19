6488ff39ab82e447410437e4110065b2996cd405
Pernille <pernille.vestberg@gmail.com>
2017-06-19 21:12:41 +0200

    Adding problemset 7.

---
+++ hw7_question1/final_files/ArrayMethodsTester.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+public class ArrayMethodsTester
+{
+    public static void main(String[] args)
+    {
+        //set up
+        String[] animals = {"ape", "dog", "zebra"};
+        ArrayMethods zoo = new ArrayMethods(animals);
+
+        //test isSorted
+        System.out.println(zoo.isSorted());
+        System.out.println("Expected: true");
+
+        String[] animals2 = {"ape", "dog", "zebra", "cat"};
+        zoo = new ArrayMethods(animals2);
+        System.out.println(zoo.isSorted());
+        System.out.println("Expected: false");
+
+        String[] animals3 = {"cat", "ape", "dog", "zebra"};
+        zoo = new ArrayMethods(animals3); ;
+        System.out.println(zoo.isSorted());
+        System.out.println("Expected: false");
+        //set up
+        String[] animals = {"cat", "ape", "dog", "horse", "zebra"};
+        ArrayMethods zoo = new ArrayMethods(animals);
+        
+        //test replaceWithLargerNeighbor
+        zoo.replaceWithLargerNeighbor();
+        System.out.println(zoo.toString());
+        System.out.println("Expected: [cat, dog, horse, zebra, zebra]");
+        
+        //test count duplicates;
+        String[] animals2 = {"cat", "cat", "dog", "horse", "zebra", "zebra"};
+        zoo = new ArrayMethods(animals2);
+        System.out.println(zoo.countDuplicates());
+        System.out.println("Expected: 2");
+       
+        String[] animals3 = {"dog", "cat", "cat", "dog", "horse", "zebra", "zebra"};
+        zoo = new ArrayMethods(animals3);
+        System.out.println(zoo.countDuplicates());
+        System.out.println("Expected: 3");        
+        
+        String[] animals4 = {"ape", "dog", "xantus",  "zebra", "cat", "yak"};
+        zoo = new ArrayMethods(animals4);
+        zoo.xyzToFront();
+        System.out.println(zoo.toString());
+        System.out.println("Expected: [xantus, zebra, yak, ape, dog, cat]");
+        
+    }
+}
