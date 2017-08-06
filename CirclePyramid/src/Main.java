73f7736afec55f0fb459bc746c8ffc6c4c809984
Pernille <pernille.vestberg@gmail.com>
2017-08-06 12:53:32 +0200

    added exercises from lesson 24. Added Final project.

---
+++ CirclePyramid/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by pernilla on 2017-08-06.
+ */
+public class Main {
+    public static void main(String[] args) {
+        CirclePyramid circlePyramid = new CirclePyramid(0,0,10,30);
+        circlePyramid.draw();
+        CirclePyramid circlePyramidSmall = new CirclePyramid(310, 0, 5, 10);
+        circlePyramidSmall.draw();
+
+    }
+}
